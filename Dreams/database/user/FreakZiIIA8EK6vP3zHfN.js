818d1a5275a107270fe02b32964b8b968640bf70
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-02 17:49:20 -0400

    Update FreakZiIIA8EK6vP3zHfN.js

--- Dreams/database/user/FreakZiIIA8EK6vP3zHfN.js
+++ Dreams/database/user/FreakZiIIA8EK6vP3zHfN.js
@@ -1,45 +1,217 @@
 document.getElementById('audioImports').innerHTML = "Audio Imports";
-      var clip = document.createElement('a');
-      clip.style = "font-size:16px;margin:0;display:block;position:relative;line-height:30px;font-weight:100";
-      clip.textContent = "Among Us Sound | Critical Sabotage Alarm"
-      clip.href = "https://indreams.me/element/oQXUZgXLUVp"
-      document.getElementById("audioClips").appendChild(clip);
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "LocoRoco - Stage Clear"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "schedule"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Pending Completion"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
 
-      var clip = document.createElement('a');
-      clip.style = "font-size:16px;margin:0;display:block;position:relative;line-height:30px;font-weight:100";
-      clip.textContent = "Astro's Playroom - PS5 Hover Track"
-      clip.href = "https://indreams.me/element/ofjhcoGppzw"
-      document.getElementById("audioClips").appendChild(clip);
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "LocoRoco - Game Over"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "schedule"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Pending Completion"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
 
-      var clip = document.createElement('a');
-      clip.style = "font-size:16px;margin:0;display:block;position:relative;line-height:30px;font-weight:100";
-      clip.textContent = "Toothbrush Timer"
-      clip.href = "https://indreams.me/element/oRiVNHFzVNi"
-      document.getElementById("audioClips").appendChild(clip);
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "LocoRoco - Dadhi Dado Da"
+      clipname.href = "https://indreams.me/element/oxbZWccHCaR"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "schedule"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Completed on July 1st, 2022"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
 
-      var clip = document.createElement('a');
-      clip.style = "font-size:16px;margin:0;display:block;position:relative;line-height:30px;font-weight:100";
-      clip.textContent = "LittleBigPlanet Recovery - i_pizz_travelling"
-      clip.href = "https://indreams.me/element/oeJCaLCywgb"
-      document.getElementById("audioClips").appendChild(clip);
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "LocoRoco - Moja's Song"
+      clipname.href = "https://indreams.me/element/oxbZWccHCaR"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "check"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Completed on July 1st, 2022"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
 
-      var clip = document.createElement('a');
-      clip.style = "font-size:16px;margin:0;display:block;position:relative;line-height:30px;font-weight:100";
-      clip.textContent = "LittleBigPlanet 2 Move Pack - move_main_2"
-      clip.href = "https://indreams.me/element/ojtGAwqPwCG"
-      document.getElementById("audioClips").appendChild(clip);
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "LocoRoco - Instrumental Main Theme"
+      clipname.href = "https://indreams.me/element/oxbZWccHCaR"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "check"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Completed on July 1st, 2022"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
 
-      var clip = document.createElement('a');
-      clip.style = "font-size:16px;margin:0;display:block;position:relative;line-height:30px;font-weight:100";
-      clip.textContent = "LocoRoco - Nyokki"
-      clip.href = "https://indreams.me/element/oAJyNLwipkL"
-      document.getElementById("audioClips").appendChild(clip);
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "LocoRoco - Nyokki"
+      clipname.href = "https://indreams.me/element/oAJyNLwipkL"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "check"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Completed on June 18th, 2022"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
 
-      var clip = document.createElement('a');
-      clip.style = "font-size:16px;margin:0;display:block;position:relative;line-height:30px;font-weight:100";
-      clip.textContent = "LocoRoco Batch 1/2"
-      clip.href = "https://indreams.me/element/oxbZWccHCaR"
-      document.getElementById("audioClips").appendChild(clip);
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "LittleBigPlanet 2 Move Pack - main_move_2"
+      clipname.href = "https://indreams.me/element/ojtGAwqPwCG"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "check"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Completed on May 28th, 2022"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
+
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "Talking Ginger - Toothbrush Timer"
+      clipname.href = "https://indreams.me/element/oRiVNHFzVNi"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "check"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Completed on April 10th, 2022"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
+
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "LittleBigPlanet Recovery - i_pizz_travelling"
+      clipname.href = "https://indreams.me/element/oMCgjuuYsjs"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "check"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Completed on April 3rd, 2022"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
+
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "Astro's Playroom - PS5 Hover Track"
+      clipname.href = "https://indreams.me/element/ofjhcoGppzw"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "check"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Completed on February 4th, 2022"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
+
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "LittleBigPlanet 3 Alpha - The Pod"
+      clipname.href = "https://indreams.me/element/oMCgjuuYsjs"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "check"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Completed on December 28th, 2021"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
+
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "Among Us - Sabotage Alarn"
+      clipname.href = "https://indreams.me/element/oQXUZgXLUVp"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "check"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Completed on September 27th, 2020"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
 
 ids.imp = "https://cdn.indreams.me/064eb4e5a0fec49042aea43eb9b492bf_5"
 ids.username = "FreakZiIIA8"
