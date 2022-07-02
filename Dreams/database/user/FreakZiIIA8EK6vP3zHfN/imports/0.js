7a2dd61a50d96367f750016dfebf4b25a81bbc97
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-02 18:39:46 -0400

    Create 0.js

---
+++ Dreams/database/user/FreakZiIIA8EK6vP3zHfN/imports/0.js
@@ -0,0 +1,17 @@
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "Among Us - Sabotage Alarn"
+      clipname.href = "https://indreams.me/element/oQXUZgXLUVp"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "check"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Completed on September 27th, 2020"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
