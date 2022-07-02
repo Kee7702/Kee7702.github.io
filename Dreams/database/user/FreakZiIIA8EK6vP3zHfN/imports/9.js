6a47e655ba683ff48c2060dcb9c676c0f36a4861
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-02 18:30:45 -0400

    Create 9.js

---
+++ Dreams/database/user/FreakZiIIA8EK6vP3zHfN/imports/9.js
@@ -0,0 +1,20 @@
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "LocoRoco - Dadhi Dado Da"
+      clipname.href = "https://indreams.me/element/oxbZWccHCaR"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "check"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Completed on July 1st, 2022"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
+var script = document.createElement('script');
+script.src = "/Dreams/database/user/FreakZiIIA8EK6vP3zHfN/imports/8.js";
+document.getElementsByTagName('head')[0].appendChild(script);
