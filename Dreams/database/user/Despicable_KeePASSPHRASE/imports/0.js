c5f5d8f73a8a696ed6e0306d7041fa44546a8492
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-02 18:59:17 -0400

    Create 0.js

---
+++ Dreams/database/user/Despicable_KeePASSPHRASE/imports/0.js
@@ -0,0 +1,17 @@
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "View All Imports"
+      clipname.href = "https://indreams.me/Despicable_Kee"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "open_in_new"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Audio Import Service"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
