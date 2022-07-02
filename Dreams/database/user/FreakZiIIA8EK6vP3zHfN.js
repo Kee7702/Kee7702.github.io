717b67d9c43aa4afc03b064af9679118d912a15f
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-02 00:22:38 -0400

    Create FreakZiIIA8EK6vP3zHfN.js

---
+++ Dreams/database/user/FreakZiIIA8EK6vP3zHfN.js
@@ -0,0 +1,53 @@
+document.getElementById('audioImports').innerHTML = "Audio Imports";
+      var clip = document.createElement('p');
+      clip.style = "font-size:16px;margin:0";
+      clip.textContent = "Among Us Sound | Critical Sabotage Alarm"
+      clip.href = "https://indreams.me/element/oQXUZgXLUVp"
+      document.getElementById("audioClips").appendChild(clip);
+
+      var clip = document.createElement('p');
+      clip.style = "font-size:16px;margin:0";
+      clip.textContent = "Astro's Playroom - PS5 Hover Track"
+      clip.href = "https://indreams.me/element/ofjhcoGppzw"
+      document.getElementById("audioClips").appendChild(clip);
+
+      var clip = document.createElement('p');
+      clip.style = "font-size:16px;margin:0";
+      clip.textContent = "Toothbrush Timer"
+      clip.href = "https://indreams.me/element/oRiVNHFzVNi"
+      document.getElementById("audioClips").appendChild(clip);
+
+      var clip = document.createElement('p');
+      clip.style = "font-size:16px;margin:0";
+      clip.textContent = "LittleBigPlanet Recovery - i_pizz_travelling"
+      clip.href = "https://indreams.me/element/oeJCaLCywgb"
+      document.getElementById("audioClips").appendChild(clip);
+
+      var clip = document.createElement('p');
+      clip.style = "font-size:16px;margin:0";
+      clip.textContent = "LittleBigPlanet 2 Move Pack - move_main_2"
+      clip.href = "https://indreams.me/element/ojtGAwqPwCG"
+      document.getElementById("audioClips").appendChild(clip);
+
+      var clip = document.createElement('p');
+      clip.style = "font-size:16px;margin:0";
+      clip.textContent = "LocoRoco - Nyokki"
+      clip.href = "https://indreams.me/element/oAJyNLwipkL"
+      document.getElementById("audioClips").appendChild(clip);
+
+      var clip = document.createElement('p');
+      clip.style = "font-size:16px;margin:0";
+      clip.textContent = "LocoRoco Batch 1/2"
+      clip.href = "https://indreams.me/element/oxbZWccHCaR"
+      document.getElementById("audioClips").appendChild(clip);
+
+ids.imp = "https://cdn.indreams.me/064eb4e5a0fec49042aea43eb9b492bf_5"
+ids.username = "FreakZiIIA8"
+ids.importCount = "11"
+ids.importLimit = "Infinite"
+ids.importTime = "Unknown"
+ids.userID = "2"
+
+var script = document.createElement('script');
+script.src = "/Dreams/database/elements.js";
+document.getElementsByTagName('head')[0].appendChild(script);
