bc140a615d45d29a59d4def78ee0624d123ddf94
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-02 18:56:23 -0400

    Update FreakZiIIA8EK6vP3zHfN.js

--- Dreams/database/user/FreakZiIIA8EK6vP3zHfN.js
+++ Dreams/database/user/FreakZiIIA8EK6vP3zHfN.js
@@ -1,7 +1,4 @@
 document.getElementById('audioImports').innerHTML = "Audio Imports";
-var script = document.createElement('script');
-script.src = "/Dreams/database/user/FreakZiIIA8EK6vP3zHfN/imports/11.js";
-document.getElementsByTagName('head')[0].appendChild(script);
 ids.imp = "https://cdn.indreams.me/064eb4e5a0fec49042aea43eb9b492bf_5"
 ids.username = "FreakZiIIA8"
 ids.importCount = "11"
@@ -10,4 +7,7 @@ ids.importTime = "Unknown"
 ids.userID = "2"
 var script = document.createElement('script');
 script.src = "/Dreams/database/elements.js";
+document.getElementsByTagName('head')[0].appendChild(script);
+var script = document.createElement('script');
+script.src = "/Dreams/database/user/FreakZiIIA8EK6vP3zHfN/imports/11.js";
 document.getElementsByTagName('head')[0].appendChild(script);
