a8d5a79d8e7d67bb58524e4e860d7ae4a8ecfaec
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-02 00:23:55 -0400

    Update Despicable_KeePASSPHRASE.js

--- Dreams/database/user/Despicable_KeePASSPHRASE.js
+++ Dreams/database/user/Despicable_KeePASSPHRASE.js
@@ -1,14 +1,10 @@
-document.getElementById('audioImports').innerHTML = "Audio Clips";
+document.getElementById('audioImports').innerHTML = "Audio Imports";
       var clip = document.createElement('p');
       clip.style = "font-size:16px;margin:0";
-      clip.textContent = "Database Script Testing"
+      clip.textContent = "View All Imports"
+      clip.href = "https://indreams.me/Despicable_Kee/creations"
       document.getElementById("audioClips").appendChild(clip);
 
-      var clip = document.createElement('audio');
-      clip.src = "/script-test.mp3";
-      clip.controls = true;
-      clip.style = "width:100%;height:20px";
-      document.getElementById("audioClips").appendChild(clip);
 ids.imp = "https://cdn.indreams.me/39ba5515c2b82cf5d347d4b25de503d3_5"
 ids.username = "Despicable_Kee"
 ids.importCount = "0"
