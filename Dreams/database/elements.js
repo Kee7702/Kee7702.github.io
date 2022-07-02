d2c642b67874ec7c173545bc074bc6f98ae43b02
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-01 22:39:07 -0400

    Create elements.js

---
+++ Dreams/database/elements.js
@@ -0,0 +1,6 @@
+document.getElementById('imp').src = (ids.imp);
+document.getElementById('username').innerHTML = (ids.username);
+document.getElementById('importCount').innerHTML = (ids.importCount);
+document.getElementById('importLimit').innerHTML = (ids.importLimit);
+document.getElementById('importTime').innerHTML = (ids.importTime);
+document.getElementById('userID').innerHTML = (ids.userID);
