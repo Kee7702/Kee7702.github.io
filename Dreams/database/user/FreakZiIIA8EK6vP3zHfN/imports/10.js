c4fc6fe684be701cd7c5d98d1736c7d0e6933574
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-02 18:28:59 -0400

    Create 10.js

---
+++ Dreams/database/user/FreakZiIIA8EK6vP3zHfN/imports/10.js
@@ -0,0 +1,19 @@
+      var clipname = document.createElement('a');
+      clipname.style = "font-size:16px;line-height:20px;color:#ccc";
+      clipname.className = "global-post-label";
+      clipname.textContent = "LocoRoco - Game Over"
+      var clipstatus = document.createElement('a');
+      clipstatus.className = "global-post-icon";
+      clipstatus.textContent = "schedule"
+      var clipdesc = document.createElement('a');
+      clipdesc.className = "global-post-details";
+      clipdesc.textContent = "Pending Completion"
+      var clipdiv = document.createElement('div');
+      clipdiv.className = "global-annoucement";
+      clipdiv.appendChild(clipname);
+      clipdiv.appendChild(clipstatus);
+      clipdiv.appendChild(clipdesc);
+      document.getElementById("audioClips").appendChild(clipdiv);
+var script = document.createElement('script');
+script.src = "/Dreams/database/user/FreakZiIIA8EK6vP3zHfN/imports/9.js";
+document.getElementsByTagName('head')[0].appendChild(script);
