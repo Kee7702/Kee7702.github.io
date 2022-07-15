6cd730480ea67170572ebab82e331981cfe19444
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-07-15 07:31:08 -0400

    Update versions.js

--- assets/js/versions.js
+++ assets/js/versions.js
@@ -9,17 +9,34 @@ var versioncodearm64v8a=null
 var versioncodex8664=null
 var versionname=document.createElement('div');versionname.className="nbt-filedownload";versionname.appendChild(versionicon);versionname.appendChild(versiontitle);var versioncodes=document.createElement('a');versioncodes.textContent="Version Codes"
 versioncodes.style="display:block;font-size:18px;line-height:22px"
-versionname.appendChild(versioncodes);if(versionlist[version].codes['armeabi-v7a']){var versioncodearmeabiv7a=document.createElement('a');versioncodearmeabiv7a.textContent=("armeabi-v7a: "+versionlist[version].codes['armeabi-v7a'])
+versionname.appendChild(versioncodes);
+if(versionlist[version].codes['armeabi-v7a'] && !versionlist[version].downloadable){var versioncodearmeabiv7a=document.createElement('a');versioncodearmeabiv7a.textContent=("armeabi-v7a: "+versionlist[version].codes['armeabi-v7a'])
 versioncodearmeabiv7a.style="display:block;font-size:14px;font-weight:100;line-height:18px"
 versionname.appendChild(versioncodearmeabiv7a)}else{}
-if(versionlist[version].codes.x86){var versioncodex86=document.createElement('a');versioncodex86.textContent=("x86: "+versionlist[version].codes.x86)
+if(versionlist[version].codes.x86 && !versionlist[version].downloadable){var versioncodex86=document.createElement('a');versioncodex86.textContent=("x86: "+versionlist[version].codes.x86)
 versioncodex86.style="display:block;font-size:14px;font-weight:100;line-height:18px"
 versionname.appendChild(versioncodex86)}else{}
-if(versionlist[version].codes['arm64-v8a']){var versioncodearm64v8a=document.createElement('a');versioncodearm64v8a.textContent=("arm64-v8a: "+versionlist[version].codes['arm64-v8a'])
+if(versionlist[version].codes['arm64-v8a'] && !versionlist[version].downloadable){var versioncodearm64v8a=document.createElement('a');versioncodearm64v8a.textContent=("arm64-v8a: "+versionlist[version].codes['arm64-v8a'])
 versioncodearm64v8a.style="display:block;font-size:14px;font-weight:100;line-height:18px"
 versionname.appendChild(versioncodearm64v8a)}else{}
-if(versionlist[version].codes.x86_64){var versioncodex8664=document.createElement('a');versioncodex8664.textContent=("x86_64: "+versionlist[version].codes.x86_64)
+if(versionlist[version].codes.x86_64 && !versionlist[version].downloadable){var versioncodex8664=document.createElement('a');versioncodex8664.textContent=("x86_64: "+versionlist[version].codes.x86_64)
 versioncodex8664.style="display:block;font-size:14px;font-weight:100;line-height:18px"
+versionname.appendChild(versioncodex8664)}else{}
+if(versionlist[version].codes['armeabi-v7a'] && versionlist[version].downloadable){var versioncodearmeabiv7a=document.createElement('a');versioncodearmeabiv7a.textContent=("armeabi-v7a: "+versionlist[version].codes['armeabi-v7a'])
+versioncodearmeabiv7a.style="display:block;font-size:14px;font-weight:100;line-height:18px;text-decoration:underline"
+versioncodearmeabiv7a.href=('/assets/apk/mcversion/' + versionlist[version].codes['armeabi-v7a'] + '.apk')
+versionname.appendChild(versioncodearmeabiv7a)}else{}
+if(versionlist[version].codes.x86 && versionlist[version].downloadable){var versioncodex86=document.createElement('a');versioncodex86.textContent=("x86: "+versionlist[version].codes.x86)
+versioncodex86.style="display:block;font-size:14px;font-weight:100;line-height:18px;text-decoration:underline"
+versioncodex86.href=('/assets/apk/mcversion/' + versionlist[version].codes.x86 + '.apk')
+versionname.appendChild(versioncodex86)}else{}
+if(versionlist[version].codes['arm64-v8a'] && versionlist[version].downloadable){var versioncodearm64v8a=document.createElement('a');versioncodearm64v8a.textContent=("arm64-v8a: "+versionlist[version].codes['arm64-v8a'])
+versioncodearm64v8a.style="display:block;font-size:14px;font-weight:100;line-height:18px;text-decoration:underline"
+versioncodearm64v8a.href=('/assets/apk/mcversion/' + versionlist[version].codes['arm64-v8a'] + '.apk')
+versionname.appendChild(versioncodearm64v8a)}else{}
+if(versionlist[version].codes.x86_64 && versionlist[version].downloadable){var versioncodex8664=document.createElement('a');versioncodex8664.textContent=("x86_64: "+versionlist[version].codes.x86_64)
+versioncodex8664.style="display:block;font-size:14px;font-weight:100;line-height:18px;text-decoration:underline"
+versioncodex8664.href=('/assets/apk/mcversion/' + versionlist[version].codes.x86_64 + '.apk')
 versionname.appendChild(versioncodex8664)}else{}
 if(versionlist[version].beta){var versionbeta=document.createElement('a');versionbeta.textContent="Beta"
 versionbeta.style="display:block"
@@ -37,17 +54,34 @@ var versioncodearm64v8a=null
 var versioncodex8664=null
 var versionname=document.createElement('div');versionname.className="nbt-filedownload";versionname.appendChild(versionicon);versionname.appendChild(versiontitle);var versioncodes=document.createElement('a');versioncodes.textContent="Version Codes"
 versioncodes.style="display:block;font-size:18px;line-height:22px"
-versionname.appendChild(versioncodes);if(versionlist[version].codes['armeabi-v7a']){var versioncodearmeabiv7a=document.createElement('a');versioncodearmeabiv7a.textContent=("armeabi-v7a: "+versionlist[version].codes['armeabi-v7a'])
+versionname.appendChild(versioncodes);
+if(versionlist[version].codes['armeabi-v7a'] && !versionlist[version].downloadable){var versioncodearmeabiv7a=document.createElement('a');versioncodearmeabiv7a.textContent=("armeabi-v7a: "+versionlist[version].codes['armeabi-v7a'])
 versioncodearmeabiv7a.style="display:block;font-size:14px;font-weight:100;line-height:18px"
 versionname.appendChild(versioncodearmeabiv7a)}else{}
-if(versionlist[version].codes.x86){var versioncodex86=document.createElement('a');versioncodex86.textContent=("x86: "+versionlist[version].codes.x86)
+if(versionlist[version].codes.x86 && !versionlist[version].downloadable){var versioncodex86=document.createElement('a');versioncodex86.textContent=("x86: "+versionlist[version].codes.x86)
 versioncodex86.style="display:block;font-size:14px;font-weight:100;line-height:18px"
 versionname.appendChild(versioncodex86)}else{}
-if(versionlist[version].codes['arm64-v8a']){var versioncodearm64v8a=document.createElement('a');versioncodearm64v8a.textContent=("arm64-v8a: "+versionlist[version].codes['arm64-v8a'])
+if(versionlist[version].codes['arm64-v8a'] && !versionlist[version].downloadable){var versioncodearm64v8a=document.createElement('a');versioncodearm64v8a.textContent=("arm64-v8a: "+versionlist[version].codes['arm64-v8a'])
 versioncodearm64v8a.style="display:block;font-size:14px;font-weight:100;line-height:18px"
 versionname.appendChild(versioncodearm64v8a)}else{}
-if(versionlist[version].codes.x86_64){var versioncodex8664=document.createElement('a');versioncodex8664.textContent=("x86_64: "+versionlist[version].codes.x86_64)
+if(versionlist[version].codes.x86_64 && !versionlist[version].downloadable){var versioncodex8664=document.createElement('a');versioncodex8664.textContent=("x86_64: "+versionlist[version].codes.x86_64)
 versioncodex8664.style="display:block;font-size:14px;font-weight:100;line-height:18px"
+versionname.appendChild(versioncodex8664)}else{}
+if(versionlist[version].codes['armeabi-v7a'] && versionlist[version].downloadable){var versioncodearmeabiv7a=document.createElement('a');versioncodearmeabiv7a.textContent=("armeabi-v7a: "+versionlist[version].codes['armeabi-v7a'])
+versioncodearmeabiv7a.style="display:block;font-size:14px;font-weight:100;line-height:18px;text-decoration:underline"
+versioncodearmeabiv7a.href=('/assets/apk/tbversion/' + versionlist[version].codes['armeabi-v7a'] + '.apk')
+versionname.appendChild(versioncodearmeabiv7a)}else{}
+if(versionlist[version].codes.x86 && versionlist[version].downloadable){var versioncodex86=document.createElement('a');versioncodex86.textContent=("x86: "+versionlist[version].codes.x86)
+versioncodex86.style="display:block;font-size:14px;font-weight:100;line-height:18px;text-decoration:underline"
+versioncodex86.href=('/assets/apk/tbversion/' + versionlist[version].codes.x86 + '.apk')
+versionname.appendChild(versioncodex86)}else{}
+if(versionlist[version].codes['arm64-v8a'] && versionlist[version].downloadable){var versioncodearm64v8a=document.createElement('a');versioncodearm64v8a.textContent=("arm64-v8a: "+versionlist[version].codes['arm64-v8a'])
+versioncodearm64v8a.style="display:block;font-size:14px;font-weight:100;line-height:18px;text-decoration:underline"
+versioncodearm64v8a.href=('/assets/apk/tbversion/' + versionlist[version].codes['arm64-v8a'] + '.apk')
+versionname.appendChild(versioncodearm64v8a)}else{}
+if(versionlist[version].codes.x86_64 && versionlist[version].downloadable){var versioncodex8664=document.createElement('a');versioncodex8664.textContent=("x86_64: "+versionlist[version].codes.x86_64)
+versioncodex8664.style="display:block;font-size:14px;font-weight:100;line-height:18px;text-decoration:underline"
+versioncodex8664.href=('/assets/apk/tbversion/' + versionlist[version].codes.x86_64 + '.apk')
 versionname.appendChild(versioncodex8664)}else{}
 if(versionlist[version].beta){var versionbeta=document.createElement('a');versionbeta.textContent="Beta"
 versionbeta.style="display:block"
