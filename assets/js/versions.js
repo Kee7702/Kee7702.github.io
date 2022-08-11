8c0d17cebf0b8198656cab4c0b7c2b2ea6c30049
Kee Productions <48420101+Kee7702@users.noreply.github.com>
2022-08-11 03:07:45 -0400

    Update versions.js

--- assets/js/versions.js
+++ assets/js/versions.js
@@ -1,4 +1,4 @@
-(async()=>{const res=await fetch(`/app/mcversion.json`);const json=await res.json();versionlist=(json);version=-1
+(async()=>{const res=await fetch(`https://raw.githubusercontent.com/Kee7702/Kee7702.github.io/database/app/mcversion.json`);const json=await res.json();versionlist=(json);version=-1
 do{version++
 var versionicon=document.createElement('i');versionicon.className="material-icons";versionicon.textContent="update"
 var versiontitle=document.createElement('a');versiontitle.textContent=versionlist[version].version_name
@@ -43,7 +43,7 @@ versionbeta.style="display:block"
 versionname.appendChild(versionbeta)}else{}
 var versioncontainer=document.createElement('div');versioncontainer.className="filedownload-container nbtcontainer";versioncontainer.appendChild(versionname);document.getElementById("mcversion").insertBefore(versioncontainer,(document.getElementById("mcversion").firstChild))
 console.log=(versionlist[version])}
-while(versionlist[version+1]);})();(async()=>{const res=await fetch(`/app/tbversion.json`);const json=await res.json();versionlist=(json);version=-1
+while(versionlist[version+1]);})();(async()=>{const res=await fetch(`https://raw.githubusercontent.com/Kee7702/Kee7702.github.io/database/app/tbversion.json`);const json=await res.json();versionlist=(json);version=-1
 do{version++
 var versionicon=document.createElement('i');versionicon.className="material-icons";versionicon.textContent="update"
 var versiontitle=document.createElement('a');versiontitle.textContent=versionlist[version].version_name
